77e513507bd6bfa585235c772943886a18b24e68
Felix23333 <67811692+Felix23333@users.noreply.github.com>
2021-08-19 14:56:09 +0800

    add some ps

---
+++ Assets/Scripts/ParticleCustomPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCustomPath : MonoBehaviour
+{
+    public string pathName;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeInOutSine, "time", time, "loopType", "loop"));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
